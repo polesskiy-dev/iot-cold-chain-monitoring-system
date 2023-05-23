03897b5952f945937fc3213d4bafdf78c03c70c9
apolisskyi <polesskiy.dev@gmail.com>
2023-05-23 11:51:31 +0100

    add wildcard for unexisting routes - 404 resoonse

---
+++ index.js
@@ -0,0 +1,10 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+module.exports.handler = serverless(app);
