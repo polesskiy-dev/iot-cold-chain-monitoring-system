74706ff764c128471afc66b1a5de44eff4c13887
apolisskyi <polesskiy.dev@gmail.com>
2023-05-23 11:44:29 +0100

    temporary get rid of index.js

---
+++ src/handlers/auth.js
@@ -0,0 +1,15 @@
+module.exports.signUp = (event, context, callback) => {
+    // create a response
+    const response = {
+        statusCode: 200,
+    }
+    callback(null, response)
+};
+
+module.exports.signIn = (event, context, callback) => {
+    // create a response
+    const response = {
+        statusCode: 200,
+    }
+    callback(null, response)
+};
